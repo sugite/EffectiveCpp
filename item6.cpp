99232a3cd7447ad60ce0948ec0ee4c7b4bc9aab6
sugite <1140625494@qq.com>
2014-10-21 10:35:21 +0800

    拒绝默认函数

---
+++ item6.cpp
@@ -0,0 +1,18 @@
+若不想使用编译器自动生成的函数，就该明确拒绝
+Explicitly disallow the use of compiler-generated functions you don't want
+
+class Uncopyable{
+	protected:
+		Uncopyable(){}
+		~Uncopyable(){}
+	private:
+		Uncopyable(const Uncopyable&);
+		Uncopyable& operator=(const Uncopyable&);
+};
+
+class HomeForSale:private Uncopyable{
+	...
+};
+
+为驳回编译器自动提供的机能，可将相应的成员函数声明为private并且不予实现
+使用像Uncopyable这样的基类也是一种做法
